1a0ca7374ef77684de46037636f5f61617ecd83f
tabletoprug <tabletoprug.og.dev@gmail.com>
2025-10-26 18:29:04 -0500

    init

---
+++ Piece.kt
@@ -0,0 +1,20 @@
+enum PieceType {
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    KING,
+    QUEEN
+}
+
+data class Piece(var type: PieceType);
+
+fun Piece.value() {
+    when(type) {
+        PieceType.PAWN -> return 1;
+        PieceType.BISHOP, PieceType.KNIGHT -> return 3;
+        PieceType.ROOK -> return 5;
+        PieceType.QUEEN -> return 9;
+        PieceType.KING -> return Int.MAX_VALUE;
+    }
+}
