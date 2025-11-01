6c7bfcd6d10ee5622caed3cca55bae5ad31bb8ae
TableTopRug <tabletoprug.og.dev@gmail.com>
2025-11-01 14:20:43 -0500

    change to oproper project structure

---
+++ src/main/kotlin/Piece.kt
@@ -0,0 +1,20 @@
+enum class PieceType {
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    KING,
+    QUEEN
+}
+
+data class Piece(var type: PieceType, val isBlack);
+
+fun Piece.value(): Int {
+    when(type) {
+        PieceType.PAWN -> return 1;
+        PieceType.BISHOP, PieceType.KNIGHT -> return 3;
+        PieceType.ROOK -> return 5;
+        PieceType.QUEEN -> return 9;
+        PieceType.KING -> return Int.MAX_VALUE;
+    }
+}
