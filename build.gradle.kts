9544deee14362312d5ce4dcbb6405f425b50499d
TableTopRug <tabletoprug.og.dev@gmail.com>
2025-11-02 11:27:25 -0600

    finally add

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    application
+//    kotlin("plugin.application") version "1.7.20"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+//    applicationContextComponent("com.example.MainKt")
+}
+
+application {
+    mainClass.set("com.example.MainKt")
+}
